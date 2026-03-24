9522c3c8e4daea7afa1d23d4d0c84299b555c946
ninasrzic <srzicn@gmail.com>
2026-03-24 19:17:06 +0100

    werw

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Worl d!");
-Console.WriteLine("Heyy girlll");
+Console.WriteLine("Heyy girll");
+Console.WriteLine("Heyy girll");
